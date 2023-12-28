13587b2d4fa78fa13c7d820675ea2d2f4e831c00
Abubakr <thefox5000@gmail.com>
2023-12-28 09:59:50 +0300

    Solve #2

---
+++ Problem #2/file.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber() {
+    int Number;
+    do
+    {
+        cout << "Enter a positive number: " << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    
+    return Number;
+}
+
+bool CheckPrime(int Number)
+{
+    for (int i = Number - 1; i > 1; i--)
+    {
+        if (Number % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void printPrimeNumbersTo(int Number) {
+    for (int i = 1; i <= Number; i++)
+    {
+        if (CheckPrime(i) == true) {
+            cout << i << endl;
+        }
+    }
+    
+}
+
+int main() { 
+    int Number = ReadPositiveNumber();
+    printPrimeNumbersTo(Number);
+
+    return 0;
+}
