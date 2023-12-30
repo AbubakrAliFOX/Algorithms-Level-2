04c52572815b8be4350154705c82e53c6dc103e3
Abubakr <thefox5000@gmail.com>
2023-12-30 09:30:05 +0300

    Inverted Number Pattern

---
+++ Problem #12/file.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(string Msg)
+{
+    int Number;
+    do
+    {
+        cout << Msg << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+void PrintInvertedPattern (int InputNumber) {
+    int Number = InputNumber;
+    for (int i = Number; i >= 1; i --) {
+        for (int j = 1; j <= i; j++ ) {
+            cout << i;
+        }
+
+        cout << "\n";
+    }
+}
+
+int main() { 
+    int Number = ReadPositiveNumber("Enter Number: ");
+    PrintInvertedPattern(Number);
+    return 0;
+}
